4c40d53b5f037683e37373f206d8d2c43e4c1f2e
Travis McKinstry <mckinstrytg@gmail.com>
2020-10-04 09:33:37 -0600

    Create relationship between players and teams, each player belongs to one team

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_152918) do
+ActiveRecord::Schema.define(version: 2020_10_04_153240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_10_04_152918) do
     t.integer "weight"
     t.string "college"
     t.string "home_state"
+    t.bigint "team_id"
+    t.index ["team_id"], name: "index_players_on_team_id"
   end
 
   create_table "teams", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_10_04_152918) do
     t.integer "year_of_inception"
   end
 
+  add_foreign_key "players", "teams"
 end
