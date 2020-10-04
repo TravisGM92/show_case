6e399a0f7615e5f14c662264eac851d9ef22570f
Travis McKinstry <mckinstrytg@gmail.com>
2020-10-04 09:25:04 -0600

    Create model test for team

---
+++ spec/models/team_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Team, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :team_name }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :year_of_inception }
+  end
+
+  # describe "relationships" do
+  #   it {should have_many :players}
+  # end
+end
