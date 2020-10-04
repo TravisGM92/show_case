7dcf2c1e6803dace0719a8e6d7e0968796d68dde
Travis McKinstry <mckinstrytg@gmail.com>
2020-10-04 09:30:38 -0600

    Create spec for new player model/table

---
+++ spec/models/player_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Player, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :height }
+    it { should validate_presence_of :weight }
+    it { should validate_presence_of :college }
+    it { should validate_presence_of :home_state }
+  end
+
+  # describe "relationships" do
+  #   it {should have_many :players}
+  # end
+end
