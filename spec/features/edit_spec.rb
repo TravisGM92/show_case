c620487eba8a81b03fc9b492eb4475c73979ca31
Travis McKinstry <mckinstrytg@gmail.com>
2020-10-14 17:35:58 -0600

    Create test for editing a team's info. Allow for link to be clicked, then be routed to a form that has form to fill out to edit the team's info

--- spec/features/edit_spec.rb
+++ spec/features/edit_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'CRUD page' do
-  it "When I visit '/skills/mvc' I see a form to fill to create a new team" do
+RSpec.describe 'Edit team info' do
+  it "When I visit '/skills/mvc/teams/:team_id/edit' I see a form to fill to create a new team" do
+      team1 = Team.create!(team_name: 'The Dogs', state: 'Colorado', year_of_inception: 1990)
 
       visit "/skills/mvc"
-
+      within("#team-#{team1.id}") do
+        click_on("Edit-team-info")
+      end
+      
       fill_in :team_name, with: "The Purple Stuff"
       fill_in :state, with: "Colorado"
       fill_in :year_of_inception, with: 1980
