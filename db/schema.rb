cbc2ee0c2db44e9aa50e86a04dc3271901f2e14f
Travis <mckinstrytg1@gmail.com>
2021-01-30 13:54:47 -0700

    Removes old DB info from previous idea

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,35 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_122_233_158) do
+ActiveRecord::Schema.define(version: 2021_01_22_233158) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'players', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'height'
-    t.integer 'weight'
-    t.string 'college'
-    t.string 'home_state'
-    t.bigint 'team_id'
-    t.string 'position'
-    t.index ['team_id'], name: 'index_players_on_team_id'
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description", default: "NO DESCRIPTION"
+    t.string "language", default: "NO LANGUAGE"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'projects', force: :cascade do |t|
-    t.string 'name'
-    t.string 'description', default: 'NO DESCRIPTION'
-    t.string 'language', default: 'NO LANGUAGE'
-    t.string 'url'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
-
-  create_table 'teams', force: :cascade do |t|
-    t.string 'team_name'
-    t.string 'state'
-    t.integer 'year_of_inception'
-  end
-
-  add_foreign_key 'players', 'teams'
 end
